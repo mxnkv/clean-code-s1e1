982bb123a84b823cc632a4f9b300d15d127f1497
mxnkv <mxnnkv@gmail.com>
2025-10-20 14:48:28 +0300

    refactor: remove tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,30 +18,34 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.classList.add("task-row");
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add("task-flag");
     //label
     var label=document.createElement("label");//label
+    label.classList.add("task-title");
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add("task-text");
     //button.edit
     var editButton=document.createElement("button");//edit button
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.classList.add("delete-img");
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add("task");
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add("task");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add("edit");
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
